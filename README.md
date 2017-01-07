c25aa71e80837856e13d98e7ffed13ef9710f878
Lucian Buzzo <lucian@gravitywell.co.uk>
2017-01-07 09:54:24 +0000

    Added JSdoc comments

--- README.md
+++ README.md
@@ -2,3 +2,86 @@
 Micro library for making AJAX requests - 857 bytes minified
 
 See a demo here https://lucianbuzzo.github.io/luigi/.
+
+# Documentation
+
+
+* [luigi](#module_luigi)
+    * [.get(url, callback)](#module_luigi.get) ⇒ <code>Object</code>
+    * [.getJSON(url, callback)](#module_luigi.getJSON) ⇒ <code>Object</code>
+    * [.getHTML(url, elementId, callback)](#module_luigi.getHTML) ⇒ <code>Object</code>
+    * [.post(url, data, callback)](#module_luigi.post) ⇒ <code>Object</code>
+
+<a name="module_luigi.get"></a>
+
+### luigi.get(url, callback) ⇒ <code>Object</code>
+**Kind**: static method of <code>[luigi](#module_luigi)</code>  
+**Summary**: Sends a GET request to a url  
+**Returns**: <code>Object</code> - - An XMLHttpRequest object.  
+**Access:** public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| url | <code>String</code> | The url to send a request to |
+| callback | <code>function</code> | callback(responseText) |
+
+**Example**  
+```js
+luigi.get('/example.html', function callback(responseText) {  console.log(responseText);});
+```
+<a name="module_luigi.getJSON"></a>
+
+### luigi.getJSON(url, callback) ⇒ <code>Object</code>
+Sends a GET request to a url that serves JSON. Data is parsed beforebeing passed to the callback function.
+
+**Kind**: static method of <code>[luigi](#module_luigi)</code>  
+**Summary**: Sends a GET request to a url that serves JSON.  
+**Returns**: <code>Object</code> - - An XMLHttpRequest object.  
+**Access:** public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| url | <code>String</code> | The url to send a request to |
+| callback | <code>function</code> | callback(responseData) |
+
+**Example**  
+```js
+luigi.getJSON('/example.html', function callback(responseData) {  console.log(responseData);});
+```
+<a name="module_luigi.getHTML"></a>
+
+### luigi.getHTML(url, elementId, callback) ⇒ <code>Object</code>
+**Kind**: static method of <code>[luigi](#module_luigi)</code>  
+**Summary**: Retrieves an HTML element from a page loaded with AJAX  
+**Returns**: <code>Object</code> - - An XMLHttpRequest object.  
+**Access:** public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| url | <code>String</code> | The url to send a request to |
+| elementId | <code>String</code> | The id of the element to search for |
+| callback | <code>function</code> | function called with the HTML element if present, else an empty string. |
+
+**Example**  
+```js
+var html = '<p><h1>FOO</h1><span id="subtitle">BAR</span></p>';var element = pickHTML(html, 'subtitle');console.log(element); // --> '<span id="subtitle">BAR</span>'
+```
+<a name="module_luigi.post"></a>
+
+### luigi.post(url, data, callback) ⇒ <code>Object</code>
+**Kind**: static method of <code>[luigi](#module_luigi)</code>  
+**Summary**: Sends a post request via AJAX  
+**Returns**: <code>Object</code> - - An XMLHttpRequest object.  
+**Access:** public  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| url | <code>String</code> | The url to send a request to |
+| data | <code>Object</code> | The data to send as the request body |
+| callback | <code>function</code> | function called with the HTML element if present, else an empty string. |
+
+**Example**  
+```js
+luigi.post('/example-endpoint', { foo: 'bar' }, function(responseText) {  console.log(responseText);});
+```
+
--- luigi.js
+++ luigi.js
@@ -1,4 +1,25 @@
-;(function() {
+!(function() {
+  /**
+   * @module luigi
+   */
+
+  /**
+   * @summary Sends a GET request to a url
+   * @memberof module:luigi
+   * @function
+   * @public
+   *
+   * @param {String} url - The url to send a request to
+   * @param {Function} callback - callback(responseText)
+   *
+   * @return {Object} - An XMLHttpRequest object.
+   *
+   * @example
+   *
+   * luigi.get('/example.html', function callback(responseText) {
+   *   console.log(responseText);
+   * });
+   */
   function get(url, callback) {
     var req = new XMLHttpRequest();
     req.onload = function() {
@@ -8,28 +29,106 @@
     req.send();
     return req;
   }
+
+
+  /**
+   * @summary Sends a GET request to a url that serves JSON.
+   * @memberof module:luigi
+   * @function
+   * @public
+   *
+   * @desc Sends a GET request to a url that serves JSON. Data is parsed before
+   * being passed to the callback function.
+   *
+   * @param {String} url - The url to send a request to
+   * @param {Function} callback - callback(responseData)
+   *
+   * @return {Object} - An XMLHttpRequest object.
+   *
+   * @example
+   *
+   * luigi.getJSON('/example.html', function callback(responseData) {
+   *   console.log(responseData);
+   * });
+   */
   function getJSON(url, callback) {
     return get(url, function getJSONCallback(data) {
       callback(JSON.parse(data));
     });
   }
+
+
+  /**
+   * @summary Retrieves a single element from a string of HTML
+   * @function
+   * @private
+   *
+   * @param {String} html - The HTML to search
+   * @param {String} elementId - The id of the element to search for
+   *
+   * @return {String} - The HTML element if present, else an empty string.
+   *
+   * @example
+   *
+   * var html = '<p><h1>FOO</h1><span id="subtitle">BAR</span></p>';
+   * var element = pickHTML(html, 'subtitle');
+   * console.log(element); // --> '<span id="subtitle">BAR</span>'
+   */
   function pickHTML(html, elementId) {
-    var regexp = new RegExp("<(?!\!)\\s*([^\\s>]+)[^>]*\\s+id\\=[\"\']" + elementId + "[\"\'][^>]*>" ,"i");
+    var regexp = new RegExp("<(?!\!)\\s*([^\\s>]+)[^>]*\\s+id\\=[\"\']" + elementId + "[\"\'][^>]*>", "i");
     var res = regexp.exec(html);
     console.log(res);
     return res ? (new RegExp('(?:(?:.(?!<\\s*' + res[1] + '[^>]*[>]))*.?<\\s*' + res[1] + '[^>]*[>](?:.(?!<\\s*\/\\s*' + res[1] + '\\s*>))*.?<\\s*\/\\s*' + res[1] + '\\s*>)*(?:.(?!<\\s*\/\\s*' + res[1] + '\\s*>))*.?', 'i')).exec(html.slice(html.indexOf(res[0]) + res[0].length))[0] || '' : '';
   }
+
+
+  /**
+   * @summary Retrieves an HTML element from a page loaded with AJAX
+   * @memberof module:luigi
+   * @function
+   * @public
+   *
+   * @param {String} url - The url to send a request to
+   * @param {String} elementId - The id of the element to search for
+   * @param {Function} callback - function called with the HTML element if present, else an empty string.
+   *
+   * @return {Object} - An XMLHttpRequest object.
+   *
+   * @example
+   *
+   * var html = '<p><h1>FOO</h1><span id="subtitle">BAR</span></p>';
+   * var element = pickHTML(html, 'subtitle');
+   * console.log(element); // --> '<span id="subtitle">BAR</span>'
+   */
   function getHTML(url, elementId, callback) {
     return get(url, function getHTMLCallback(data) {
       callback(pickHTML(data, elementId));
     });
   }
+
+
+  /**
+   * @summary Sends a post request via AJAX
+   * @memberof module:luigi
+   * @function
+   * @public
+   *
+   * @param {String} url - The url to send a request to
+   * @param {Object} data - The data to send as the request body
+   * @param {Function} callback - function called with the HTML element if present, else an empty string.
+   *
+   * @return {Object} - An XMLHttpRequest object.
+   *
+   * @example
+   * luigi.post('/example-endpoint', { foo: 'bar' }, function(responseText) {
+   *   console.log(responseText);
+   * });
+   */
   function post(url, data, callback) {
     var req = new XMLHttpRequest();
     req.onload = function() {
       callback(req.responseText);
     };
-    var segments = [];
     req.open('post', url, true);
     req.setRequestHeader('Content-Type', 'application\/json');
     req.send(JSON.stringify(data));
